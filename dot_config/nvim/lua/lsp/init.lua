6d855bd3e628e408a70661c83612cee9fd701b18
Daniel Pieper <daniel@daniel-pieper.com>
2020-08-15 11:55:32 +0200

    Configure nvim lsp

--- dot_config/nvim/lua/lsp/init.lua
+++ dot_config/nvim/lua/lsp/init.lua
@@ -6,19 +6,23 @@ local configs = require('nvim_lsp/configs')
 local util = require('nvim_lsp/util')
 
 local on_attach = function(client, bufnr)
+  -- Set the omnifunc for this buffer.
+  vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+
+  -- Keybindings for LSPs
+  local opts = { noremap=true, silent=true }
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>gh", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "<c-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>gt", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>g0", "<cmd>lua vim.lsp.buf.document_symbol()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>gW", "<cmd>lua vim.lsp.buf.workspace_symbol()<CR>", opts)
+
   -- lsp_status.on_attach(client, bufnr)
   diagnostic.on_attach(client, bufnr)
   completion.on_attach(client, bufnr)
-
-  -- Keybindings for LSPs
-  vim.fn.nvim_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", {noremap = true, silent = true})
-  vim.fn.nvim_set_keymap("n", "gh", "<cmd>lua vim.lsp.buf.hover()<CR>", {noremap = true, silent = true})
-  vim.fn.nvim_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.implementation()<CR>", {noremap = true, silent = true})
-  vim.fn.nvim_set_keymap("n", "<c-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", {noremap = true, silent = true})
-  vim.fn.nvim_set_keymap("n", "1gD", "<cmd>lua vim.lsp.buf.type_definition()<CR>", {noremap = true, silent = true})
-  vim.fn.nvim_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", {noremap = true, silent = true})
-  vim.fn.nvim_set_keymap("n", "g0", "<cmd>lua vim.lsp.buf.document_symbol()<CR>", {noremap = true, silent = true})
-  vim.fn.nvim_set_keymap("n", "gW", "<cmd>lua vim.lsp.buf.workspace_symbol()<CR>", {noremap = true, silent = true})
 end
 -- lsp_status.register_progress()
 -- lsp_status.config({
@@ -30,33 +34,52 @@ end
 --   indicator_ok = 'ok',
 -- })
 
-nvim_lsp.clangd.setup({
+nvim_lsp.intelephense.setup{
+  on_attach = on_attach,
+  -- capabilities = lsp_status.capabilities
+  -- init_options = {
+  --   licenceKey = get from 1password
+  -- },
+}
+
+nvim_lsp.jsonls.setup{
+  on_attach = on_attach,
+  -- capabilities = lsp_status.capabilities
+}
+
+nvim_lsp.clangd.setup{
   -- callbacks = lsp_status.extensions.clangd.setup(),
   init_options = {
     clangdFileStatus = true
   },
   on_attach = on_attach,
   -- capabilities = lsp_status.capabilities
-})
+}
+
 nvim_lsp.pyls.setup{
   on_attach = on_attach,
   -- capabilities = lsp_status.capabilities
 }
+
 nvim_lsp.sumneko_lua.setup{
   on_attach = on_attach,
 }
+
 nvim_lsp.vimls.setup{
   on_attach = on_attach,
   -- capabilities = lsp_status.capabilities
 }
+
 nvim_lsp.tsserver.setup{
   on_attach = on_attach,
   -- capabilities = lsp_status.capabilities
 }
+
 nvim_lsp.html.setup{
   on_attach = on_attach,
   -- capabilities = lsp_status.capabilities
 }
+
 nvim_lsp.cssls.setup{
   on_attach = on_attach,
   -- capabilities = lsp_status.capabilities,
@@ -77,10 +100,12 @@ nvim_lsp.rust_analyzer.setup{
   on_attach = on_attach,
   -- capabilities = lsp_status.capabilities
 }
+
 nvim_lsp.elixirls.setup{
   on_attach = on_attach,
   -- capabilities = lsp_status.capabilities
 }
+
 nvim_lsp.gopls.setup{
   on_attach = on_attach,
   -- capabilities = lsp_status.capabilities
