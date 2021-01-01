170babeda69bb3a2c8caff30fb2393f076f4f70c
Daniel Pieper <daniel@daniel-pieper.com>
2021-01-01 18:46:30 +0100

    Fix nvim-tree, comment intelephense docker

--- dot_config/nvim/lua/plugins/nvim-tree.lua
+++ dot_config/nvim/lua/plugins/nvim-tree.lua
@@ -1,11 +1,11 @@
 local map = require("utils").map
 
-vim.api.nvim_set_var('lua_tree_indent_markers', 1)
-vim.api.nvim_set_var('lua_tree_bindings', {
+vim.api.nvim_set_var('nvim_tree_indent_markers', 1)
+vim.api.nvim_set_var('nvim_tree_bindings', {
   edit = {'<CR>', 'o', 'l'},
 })
 
-vim.api.nvim_set_var('lua_tree_icons', {
+vim.api.nvim_set_var('nvim_tree_icons', {
   default = '',
   symlink = '',
   git = {
@@ -17,12 +17,12 @@ vim.api.nvim_set_var('lua_tree_icons', {
   },
 })
 
-vim.api.nvim_set_var('lua_tree_side', 'right')
-vim.api.nvim_set_var('lua_tree_width', 50)
-vim.api.nvim_set_var('lua_tree_ignore', { '.git', 'node_modules', '.cache' })
-vim.api.nvim_set_var('lua_tree_auto_close', 1)
-vim.api.nvim_set_var('lua_tree_quit_on_open', 1)
-vim.api.nvim_set_var('lua_tree_git_hl', 1)
+vim.api.nvim_set_var('nvim_tree_side', 'right')
+vim.api.nvim_set_var('nvim_tree_width', 50)
+vim.api.nvim_set_var('nvim_tree_ignore', { '.git', 'node_modules', '.cache' })
+vim.api.nvim_set_var('nvim_tree_auto_close', 1)
+vim.api.nvim_set_var('nvim_tree_quit_on_open', 1)
+vim.api.nvim_set_var('nvim_tree_git_hl', 1)
 
-map('n', '<F3>', ':LuaTreeToggle<CR>', { silent = true })
-map('n', '<Leader><F3>', ':LuaTreeFindFile<CR>', { silent = true })
+map('n', '<F3>', ':NvimTreeToggle<CR>', { silent = true })
+map('n', '<Leader><F3>', ':NvimTreeFindFile<CR>', { silent = true })
