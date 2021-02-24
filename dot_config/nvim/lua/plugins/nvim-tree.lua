c867bcdc9711c9ebb9b1fba670141e248e90635d
Daniel Pieper <daniel.pieper@personio.de>
2021-02-24 12:33:50 +0100

    Update nvim-tree configuration

--- dot_config/nvim/lua/plugins/nvim-tree.lua
+++ dot_config/nvim/lua/plugins/nvim-tree.lua
@@ -1,28 +1,19 @@
 local map = require("utils").map
 
-vim.api.nvim_set_var('nvim_tree_indent_markers', 1)
 vim.api.nvim_set_var('nvim_tree_bindings', {
-  edit = {'<CR>', 'o', 'l'},
+  ["l"] = ":lua require('nvim-tree').on_keypress('edit')<CR>",
 })
+
+vim.api.nvim_set_var('nvim_tree_side', 'right')
+vim.api.nvim_set_var('nvim_tree_width', 60)
+vim.api.nvim_set_var('nvim_tree_ignore', { '.git', 'node_modules', '.cache' })
+vim.api.nvim_set_var('nvim_tree_auto_close', 1)
+vim.api.nvim_set_var('nvim_tree_quit_on_open', 1)
 
 vim.api.nvim_set_var('nvim_tree_icons', {
   default = '',
   symlink = '',
-  git = {
-    unstaged = "✚",
-    staged = "✚",
-    unmerged = "≠",
-    renamed = "≫",
-    untracked = "★"
-  },
 })
-
-vim.api.nvim_set_var('nvim_tree_side', 'right')
-vim.api.nvim_set_var('nvim_tree_width', 50)
-vim.api.nvim_set_var('nvim_tree_ignore', { '.git', 'node_modules', '.cache' })
-vim.api.nvim_set_var('nvim_tree_auto_close', 1)
-vim.api.nvim_set_var('nvim_tree_quit_on_open', 1)
-vim.api.nvim_set_var('nvim_tree_git_hl', 1)
 
 map('n', '<F3>', ':NvimTreeToggle<CR>', { silent = true })
 map('n', '<Leader><F3>', ':NvimTreeFindFile<CR>', { silent = true })
