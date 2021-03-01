caf75c06e18f2a3bdca0bacc35cc5e4a1e3e7e3f
Daniel Pieper <daniel.pieper@personio.de>
2021-03-01 09:18:35 +0100

    Fix telescope mapping for darwin ...

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -1,8 +1,17 @@
 local telescope = require('telescope')
+local actions = require('telescope.actions')
 local map = require("utils").map
 
 telescope.setup{
   defaults = {
+    mappings = {
+      i = {
+        ["œ"] = actions.send_selected_to_qflist + actions.open_qflist, -- <M-q> on darwin...
+      },
+      n = {
+        ["œ"] = actions.send_selected_to_qflist + actions.open_qflist, -- <M-q> on darwin...
+      },
+    },
     -- winblend = 20;
     width = 0.8;
     show_line = false;
@@ -33,7 +42,8 @@ telescope.setup{
 -- })
 
 map("n", "<C-p>", ":lua require('telescope.builtin').git_files{}<CR>", {silent = true})
-map("n", "<A-p>", ":lua require('telescope.builtin').find_files{}<CR>", {silent = true})
+map("n", "<M-p>", ":lua require('telescope.builtin').find_files{}<CR>", {silent = true})
+map("n", "π", ":lua require('telescope.builtin').find_files{}<CR>", {silent = true}) -- find files on darwin...
 map("n", "<BS>", ":lua require('telescope.builtin').command_history{}<CR>", {silent = true})
 map("n", "<C-f>", ":lua require('telescope.builtin').live_grep{}<CR>", {silent = true})
 map("n", "<C-SPACE>", ":lua require('telescope.builtin').buffers{show_all_buffers = true}<CR>", {silent = true}) -- Show unloaded buffers aswell
