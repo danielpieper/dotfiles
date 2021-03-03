82da3ebc569dffe5e3dd1a84c6d12d695e5def1a
Daniel Pieper <daniel.pieper@personio.de>
2021-03-03 09:08:53 +0100

    Update firenvim config

--- dot_config/nvim/lua/plugins/firenvim.lua
+++ dot_config/nvim/lua/plugins/firenvim.lua
@@ -1,6 +1,11 @@
 local utils = require("utils")
 local g = vim.g
 
+-- Default trigger: <apple>E
+-- see https://github.com/glacambre/firenvim/wiki/Filetypes-for-websites-&-more
+
+-- takeover that can be set to always, empty, never, nonempty or once
+
 g.firenvim_config = {
   globalSettings = {
     alt = 'all',
@@ -13,7 +18,18 @@ g.firenvim_config = {
       takeover = 'never',
     },
     [".*"] = {
+      cmdline = 'firenvim',
+      priority = 0,
+      selector = 'textarea:not([readonly]):not([class="handsontableInput"]), div[role="textbox"]',
+      takeover = 'once',
+    },
+    [".*notion\\.so.*"] = {
+      priority = 9,
       takeover = 'never',
+    },
+    [".*docs\\.google\\.com.*"] = {
+      priority = 9,
+      takeover = 'never'
     },
   }
 }
@@ -48,32 +64,59 @@ function! s:IsFirenvimActive(event) abort
   return has_key(l:ui, 'client') && has_key(l:ui.client, 'name') && l:ui.client.name =~? 'Firenvim'
 endfunction
 
-function! SetLines(timer) abort
-    let wheight = winheight(0)
-    if wheight < 20
-      set lines=20
-    elseif wheight > 50
-      set lines=50
-    endif
-
-    let wwidth = winwidth(0)
-    if wwidth < 40
-      set columns=40
-    elseif wwidth > 100
-      set columns=100
-    endif
-
-    set wrap
-endfunction
-
 function! OnUIEnter(event) abort
   if s:IsFirenvimActive(a:event)
     set laststatus=0
-    call timer_start(800, function("SetLines"))
     autocmd BufEnter *atlassian.net_*.txt set filetype=html
+    autocmd BufEnter *gitlab.*.txt set filetype=markdown
+    autocmd BufEnter *github.*.txt set filetype=markdown
   endif
 endfunction
 autocmd UIEnter * call OnUIEnter(deepcopy(v:event))
     ]])
   end
 )
+
+-- utils.augroup(
+--   'firenvim',
+--   function ()
+--     vim.cmd([[
+-- function! s:IsFirenvimActive(event) abort
+--   if !exists('*nvim_get_chan_info')
+--     return 0
+--   endif
+--   let l:ui = nvim_get_chan_info(a:event.chan)
+--   return has_key(l:ui, 'client') && has_key(l:ui.client, 'name') && l:ui.client.name =~? 'Firenvim'
+-- endfunction
+
+-- function! SetLines(timer) abort
+--     let wheight = winheight(0)
+--     if wheight < 20
+--       set lines=20
+--     elseif wheight > 50
+--       set lines=50
+--     endif
+
+--     let wwidth = winwidth(0)
+--     if wwidth < 40
+--       set columns=40
+--     elseif wwidth > 100
+--       set columns=100
+--     endif
+
+--     set wrap
+-- endfunction
+
+-- function! OnUIEnter(event) abort
+--   if s:IsFirenvimActive(a:event)
+--     set laststatus=0
+--     call timer_start(800, function("SetLines"))
+--     autocmd BufEnter *atlassian.net_*.txt set filetype=html
+--     autocmd BufEnter *gitlab.*.txt set filetype=markdown
+--     autocmd BufEnter *github.*.txt set filetype=markdown
+--   endif
+-- endfunction
+-- autocmd UIEnter * call OnUIEnter(deepcopy(v:event))
+--     ]])
+--   end
+-- )
