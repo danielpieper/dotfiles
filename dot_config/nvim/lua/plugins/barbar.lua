da4886e16e650b5c03c45571c520245804be0c23
Daniel Pieper <daniel@daniel-pieper.com>
2021-03-03 06:43:37 +0100

    Update nvim barbar configuration

--- dot_config/nvim/lua/plugins/barbar.lua
+++ dot_config/nvim/lua/plugins/barbar.lua
@@ -34,25 +34,52 @@ map('n', '<Leader>Q', ":BufferWipeout<CR>", {silent = true})
 
 map('n', 'Q', ":BufferCloseAllButCurrent<CR>", {silent = true})
 map('n', 'Ql', ":BufferCloseBuffersRight<CR>", {silent = true})
+map('n', 'Qh', ":BufferCloseBuffersLeft<CR>", {silent = true})
 
 vim.g.bufferline = {
-  shadow = false, -- Show a shadow over the editor in buffer-pick mode
-  animation = false, -- Enable/disable animations
-  icons = true, -- Enable/disable icons
-  closable = false, -- Enable/disable close button
+  -- Enable/disable animations
+  animation = false,
+
+  -- Enable/disable icons
+  -- if set to 'numbers', will show buffer index in the tabline
+  -- if set to 'both', will show buffer index and icons in the tabline
+  icons = 'both',
+
+  -- If set, the icon color will follow its corresponding buffer
+  -- highlight group. By default, the Buffer*Icon group is linked to the
+  -- Buffer* group (see Highlighting below). Otherwise, it will take its
+  -- default value as defined by devicons.
+  icon_custom_colors = false,
+
+  -- Configure icons on the bufferline.
+  icon_separator_active = '▎',
+  icon_separator_inactive = '▎',
+  icon_close_tab = '',
+  icon_close_tab_modified = '●',
+
+  -- Enable/disable close button
+  closable = false,
+
   -- Enables/disable clickable tabs
-  --  - left-click: go to buffer
-  --  - middle-click: delete buffer
+  -- left-click: go to buffer
+  -- middle-click: delete buffer
   clickable = true,
+
   -- If set, the letters for each buffer in buffer-pick mode will be
   -- assigned based on their name. Otherwise or in case all letters are
   -- already assigned, the behavior is to assign letters in order of
   -- usability (see order below)
   semantic_letters = true,
+
   -- New buffer letters are assigned in this order. This order is
   -- optimal for the qwerty keyboard layout but might need adjustement
   -- for other layouts.
   letters = 'asdfjkl;ghnmxcbziowerutyqpASDFJKLGHNMXCBZIOWERUTYQP',
-  maximum_padding = 4, -- Sets the maximum padding width with which to surround each tab
+
+  -- Sets the maximum padding width with which to surround each tab
+  maximum_padding = 4,
+
+  -- Enable/disable auto-hiding the tab bar when there is a single buffer
+  auto_hide = false,
 }
 
