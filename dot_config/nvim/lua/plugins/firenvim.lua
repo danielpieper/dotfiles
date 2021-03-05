16676d78b71a2f9984d1d847a8b52e2d95b854c6
Daniel Pieper <daniel.pieper@personio.de>
2021-03-05 12:27:26 +0100

    Update nvim window height/width

--- dot_config/nvim/lua/plugins/firenvim.lua
+++ dot_config/nvim/lua/plugins/firenvim.lua
@@ -64,10 +64,30 @@ function! s:IsFirenvimActive(event) abort
   return has_key(l:ui, 'client') && has_key(l:ui.client, 'name') && l:ui.client.name =~? 'Firenvim'
 endfunction
 
+function! SetLines(timer) abort
+    let wheight = winheight(0)
+    if wheight < 15
+      set lines=15
+    elseif wheight > 50
+      set lines=50
+    endif
+
+    let wwidth = winwidth(0)
+    if wwidth < 30
+      set columns=30
+    elseif wwidth > 100
+      set columns=100
+    endif
+
+    set wrap
+endfunction
+
 function! OnUIEnter(event) abort
   if s:IsFirenvimActive(a:event)
     set laststatus=0
     let g:bufferline.auto_hide=1
+
+    call timer_start(800, function("SetLines"))
 
     autocmd BufEnter *atlassian.net_*.txt set filetype=html
     autocmd BufEnter *gitlab.*.txt set filetype=markdown
@@ -78,47 +98,3 @@ autocmd UIEnter * call OnUIEnter(deepcopy(v:event))
     ]])
   end
 )
-
--- utils.augroup(
---   'firenvim',
---   function ()
---     vim.cmd([[
--- function! s:IsFirenvimActive(event) abort
---   if !exists('*nvim_get_chan_info')
---     return 0
---   endif
---   let l:ui = nvim_get_chan_info(a:event.chan)
---   return has_key(l:ui, 'client') && has_key(l:ui.client, 'name') && l:ui.client.name =~? 'Firenvim'
--- endfunction
-
--- function! SetLines(timer) abort
---     let wheight = winheight(0)
---     if wheight < 20
---       set lines=20
---     elseif wheight > 50
---       set lines=50
---     endif
-
---     let wwidth = winwidth(0)
---     if wwidth < 40
---       set columns=40
---     elseif wwidth > 100
---       set columns=100
---     endif
-
---     set wrap
--- endfunction
-
--- function! OnUIEnter(event) abort
---   if s:IsFirenvimActive(a:event)
---     set laststatus=0
---     call timer_start(800, function("SetLines"))
---     autocmd BufEnter *atlassian.net_*.txt set filetype=html
---     autocmd BufEnter *gitlab.*.txt set filetype=markdown
---     autocmd BufEnter *github.*.txt set filetype=markdown
---   endif
--- endfunction
--- autocmd UIEnter * call OnUIEnter(deepcopy(v:event))
---     ]])
---   end
--- )
