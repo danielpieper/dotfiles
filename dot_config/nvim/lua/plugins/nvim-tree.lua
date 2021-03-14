0b47e6b3b397568867395b4020d4379f2894c491
Daniel Pieper <daniel@daniel-pieper.com>
2021-03-14 09:00:29 +0100

    Update lspkind-nvim and nvim-tree config

--- dot_config/nvim/lua/plugins/nvim-tree.lua
+++ dot_config/nvim/lua/plugins/nvim-tree.lua
@@ -4,11 +4,13 @@ vim.api.nvim_set_var('nvim_tree_bindings', {
   ["l"] = ":lua require('nvim-tree').on_keypress('edit')<CR>",
 })
 
-vim.api.nvim_set_var('nvim_tree_side', 'right')
-vim.api.nvim_set_var('nvim_tree_width', 60)
-vim.api.nvim_set_var('nvim_tree_ignore', { '.git', 'node_modules', '.cache' })
-vim.api.nvim_set_var('nvim_tree_auto_close', 1)
-vim.api.nvim_set_var('nvim_tree_quit_on_open', 1)
+vim.g.nvim_tree_side = "right"
+vim.g.nvim_tree_width = 60
+vim.g.nvim_tree_ignore = { '.git', 'node_modules', '.cache' }
+vim.g.nvim_tree_auto_close = 1
+vim.g.nvim_tree_quit_on_open = 1
+vim.g.nvim_tree_indent_markers = 1
+vim.g.nvim_tree_git_hl = 1
 
 vim.api.nvim_set_var('nvim_tree_icons', {
   default = '',
