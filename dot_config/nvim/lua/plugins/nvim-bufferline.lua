0ea379df10cdc867aa51079ef2e6c56e85710c64
Daniel Pieper <daniel.pieper@personio.de>
2021-03-15 14:52:23 +0100

    Bufferline configuration

--- dot_config/nvim/lua/plugins/nvim-bufferline.lua
+++ dot_config/nvim/lua/plugins/nvim-bufferline.lua
@@ -4,14 +4,14 @@ local bl = require("bufferline")
 bl.setup {
     options = {
         view = "multiwindow",
+        numbers = "ordinal", -- "none" | "ordinal" | "buffer_id",
+        number_style = "", -- "superscript" | "",
         -- modified_icon = '✥',
         show_buffer_close_icons = false,
         mappings = true,
         always_show_bufferline = true,
-        -- sort_by = "directory",
-        numbers = "none", -- "none" | "ordinal" | "buffer_id",
-        number_style = "", -- "superscript" | "",
         sort_by = 'directory',
+        show_tab_indicators = true,
     },
     -- highlights = {
     --     bufferline_tab = {guifg = '#383D44', guibg = '#151718'},
@@ -21,5 +21,5 @@ bl.setup {
 }
 
 map('n', '<Leader>gb', ':BufferLinePick<CR>', {silent = true})
-map('n', '[b', ':BufferLineCycleNext<CR>', {silent = true})
-map('n', 'b]', ':BufferLineCyclePrev<CR>', {silent = true})
+map('n', '[b', ':BufferLineCyclePrev<CR>', {silent = true})
+map('n', ']b', ':BufferLineCycleNext<CR>', {silent = true})
