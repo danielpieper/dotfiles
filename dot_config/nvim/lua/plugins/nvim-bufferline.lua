4987e83416ffc820ab3f1035eadf74548ab901d4
Daniel Pieper <daniel@daniel-pieper.com>
2021-03-16 00:17:17 +0100

    Use nvim-bufferline

--- dot_config/nvim/lua/plugins/nvim-bufferline.lua
+++ dot_config/nvim/lua/plugins/nvim-bufferline.lua
@@ -3,15 +3,22 @@ local bl = require("bufferline")
 
 bl.setup {
     options = {
-        view = "multiwindow",
-        numbers = "ordinal", -- "none" | "ordinal" | "buffer_id",
-        number_style = "", -- "superscript" | "",
-        -- modified_icon = '✥',
-        show_buffer_close_icons = false,
-        mappings = true,
-        always_show_bufferline = true,
-        sort_by = 'directory',
-        show_tab_indicators = true,
+      view = "multiwindow",
+      numbers = "ordinal", -- "none" | "ordinal" | "buffer_id",
+      number_style = "", -- "superscript" | "",
+      modified_icon = "●",
+      show_buffer_close_icons = false,
+      mappings = true,
+      always_show_bufferline = true,
+      -- sort_by = "directory",
+      sort_by = 'directory',
+      show_tab_indicators = true,
+      separator_style = "thin",
+
+      diagnostics_indicator = function(count, level)
+          local icon = level:match("error") and " " or ""
+          return " " .. icon .. count
+      end
     },
     -- highlights = {
     --     bufferline_tab = {guifg = '#383D44', guibg = '#151718'},
@@ -20,6 +27,6 @@ bl.setup {
     -- }
 }
 
-map('n', '<Leader>gb', ':BufferLinePick<CR>', {silent = true})
+map('n', '<Leader>bb', ':BufferLinePick<CR>', {silent = true})
 map('n', '[b', ':BufferLineCyclePrev<CR>', {silent = true})
 map('n', ']b', ':BufferLineCycleNext<CR>', {silent = true})
