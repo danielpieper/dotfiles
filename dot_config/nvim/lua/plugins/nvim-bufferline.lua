1fcf106fed6b4ca2c97315b150114da64bdbbd1f
Daniel Pieper <daniel@daniel-pieper.com>
2021-03-16 18:01:11 +0100

    Update bufferline highlights

--- dot_config/nvim/lua/plugins/nvim-bufferline.lua
+++ dot_config/nvim/lua/plugins/nvim-bufferline.lua
@@ -3,22 +3,37 @@ local bl = require("bufferline")
 
 bl.setup {
     options = {
-      view = "multiwindow",
-      numbers = "ordinal", -- "none" | "ordinal" | "buffer_id",
-      number_style = "", -- "superscript" | "",
-      modified_icon = "●",
-      show_buffer_close_icons = false,
-      mappings = true,
-      always_show_bufferline = true,
-      -- sort_by = "directory",
-      sort_by = 'directory',
-      show_tab_indicators = true,
-      separator_style = "thin",
+        view = "multiwindow",
+        numbers = "ordinal", -- "none" | "ordinal" | "buffer_id",
+        number_style = "", -- "superscript" | "",
+        modified_icon = "●",
+        show_buffer_close_icons = false,
+        mappings = true,
+        always_show_bufferline = true,
+        -- sort_by = "directory",
+        sort_by = 'directory',
+        show_tab_indicators = true,
+        separator_style = "thin",
 
-      diagnostics_indicator = function(count, level)
-          local icon = level:match("error") and " " or ""
-          return " " .. icon .. count
-      end
+        diagnostics = "nvim_lsp",
+        diagnostics_indicator = function(count, level)
+            local icon = level:match("error") and " " or ""
+            return " " .. icon .. count
+        end,
+        highlights = {
+            warning = {
+                guisp = "Yellow",
+            },
+            warning_selected = {
+                guifg = "Yellow",
+            },
+            error = {
+                guisp = "Red",
+            },
+            error_selected = {
+                guifg = "Red",
+            },
+        };
     },
     -- highlights = {
     --     bufferline_tab = {guifg = '#383D44', guibg = '#151718'},
