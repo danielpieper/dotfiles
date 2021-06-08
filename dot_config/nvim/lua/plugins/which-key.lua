281de55b4d74f3861f415b602f8c39d171cb4c7d
Daniel Pieper <daniel.pieper@personio.de>
2021-06-08 16:55:44 +0200

    Add nvim which-key

---
+++ dot_config/nvim/lua/plugins/which-key.lua
@@ -0,0 +1,7 @@
+local whichkey = require('which-key')
+
+whichkey.setup {
+  -- your configuration comes here
+  -- or leave it empty to use the default settings
+  -- refer to the configuration section below
+}
