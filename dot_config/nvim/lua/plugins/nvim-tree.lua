715fdba53714003c1cbd6ed9090cf507a7d1e893
Daniel Pieper <daniel.pieper@personio.de>
2021-07-01 08:52:54 +0200

    Update nvim-tree key binding

--- dot_config/nvim/lua/plugins/nvim-tree.lua
+++ dot_config/nvim/lua/plugins/nvim-tree.lua
@@ -2,7 +2,7 @@ local map = require("utils").wkmap
 local tree_cb = require('nvim-tree.config').nvim_tree_callback
 
 vim.api.nvim_set_var('nvim_tree_bindings', {
-  ["l"] = tree_cb("edit"),
+  { key = "l", cb = tree_cb("edit") },
 })
 
 vim.g.nvim_tree_side = "right"
