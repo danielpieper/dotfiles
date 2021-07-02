8b091ec02493e33f4e681c9372c8df3cc96797e6
Daniel Pieper <daniel.pieper@personio.de>
2021-07-02 09:52:01 +0200

    Update telescope config

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -12,34 +12,8 @@ telescope.setup{
         ["œ"] = actions.send_selected_to_qflist + actions.open_qflist, -- <M-q> on darwin...
       },
     },
-    -- winblend = 20;
-    width = 0.8;
-    show_line = false;
-    -- prompt_prefix = 'TS Symbols>';
-    prompt_title = '';
-    results_title = '';
-    preview_title = '';
-    -- borderchars = {
-    --   prompt = {'▀', '▐', '▄', '▌', '▛', '▜', '▟', '▙' };
-    --   results = {'▀', '▐', '▄', '▌', '▛', '▜', '▟', '▙' };
-    --   preview = {'▀', '▐', '▄', '▌', '▛', '▜', '▟', '▙' };
-    -- };
   }
 }
-
--- local dropdown_theme = require('telescope.themes').get_dropdown({
---   results_height = 20;
---   winblend = 20;
---   width = 0.8;
---   prompt_title = '';
---   prompt_prefix = 'Files>';
---   previewer = false;
---   borderchars = {
---     prompt = {'▀', '▐', '▄', '▌', '▛', '▜', '▟', '▙' };
---     results = {' ', '▐', '▄', '▌', '▌', '▐', '▟', '▙' };
---     preview = {'▀', '▐', '▄', '▌', '▛', '▜', '▟', '▙' };
---   };
--- })
 
 map("n", "<C-p>", ":lua require('telescope.builtin').git_files{}<CR>", 'Find Git Files', {silent = true})
 map("n", "<M-p>", ":lua require('telescope.builtin').find_files{}<CR>", 'Find Files', {silent = true})
