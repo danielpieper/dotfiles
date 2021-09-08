b66bc7d51fe2838d6a04f175545b5746fac0091c
Daniel Pieper <daniel@daniel-pieper.com>
2021-09-08 08:07:57 +0200

    Replace nvim-compe with nvim-cmp, snippets.nvim with vim-vsnip

---
+++ dot_config/nvim/lua/plugins/nvim-cmp.lua
@@ -0,0 +1,48 @@
+local cmp = require('cmp')
+
+cmp.setup {
+  snippet = {
+    expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body)
+    end,
+  },
+  mapping = {
+    ['<S-Tab>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 's' }),
+    ['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's' }),
+    ['<C-n>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-p>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.close(),
+    ['<CR>'] = cmp.mapping.confirm({
+      behavior = cmp.ConfirmBehavior.Insert,
+      select = true,
+    }),
+  },
+  formatting = {
+    format = function(entry, vim_item)
+      -- fancy icons and a name of kind
+      vim_item.kind = require("lspkind").presets.default[vim_item.kind] .. " " .. vim_item.kind
+
+      -- set a name for each source
+      vim_item.menu = ({
+        nvim_lsp = "[LSP]",
+        vsnip = "[Snippet]",
+        buffer = "[Buffer]",
+        path = "[Path]",
+        calc = "[Calc]",
+        nvim_lua = "[Lua]",
+        emoji = "[Emoji]",
+      })[entry.source.name]
+      return vim_item
+    end,
+  },
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'vsnip' },
+    { name = 'buffer' },
+    { name = 'path' },
+    { name = 'calc' },
+    { name = 'nvim_lua' },
+    { name = 'emoji' },
+  },
+}
